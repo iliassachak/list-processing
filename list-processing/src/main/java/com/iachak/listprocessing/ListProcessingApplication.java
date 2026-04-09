7f7f849014ae3f119bb33c6e176fd0c5b22e0f50
PC_Travail <iliassachak@gmail.com>
2026-04-09 09:51:20 +0100

    1st

---
+++ list-processing/src/main/java/com/iachak/listprocessing/ListProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.iachak.listprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListProcessingApplication.class, args);
+	}
+
+}
---
+++ list-processing/src/test/java/com/iachak/listprocessing/ListProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iachak.listprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
